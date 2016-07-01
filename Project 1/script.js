8a005b70aa17d519c828b7619e3582f58eb31201
Emaestas <emaestas@youth.dhftc.org>
2016-07-01 14:53:36 -0400

    Added html, js and css files from brackets

---
+++ Project 1/script.js
@@ -0,0 +1,13 @@
+var myButtons = document.getElementById('button_one');
+
+function alertbutton() {
+    prompt("Hello");
+}
+
+myButtons.addEventListener('click', alertbutton, false);
+    
+
+var ethan = function () {
+    alert('goodbye');
+};
+
